b008801487b94892a4134a4d03f411caa785b340
HarshitaShekhawat6 <shekhawatharshita219@gmail.com>
2025-06-26 08:27:02 +0530

    Updated home component UI and layout

--- weather.js
+++ weather.js
@@ -34,19 +34,19 @@ async function checkWeather(city)
 
   switch(weather_data.weather[0].main){
     case'Clouds':
-    weather_img.src="./assests/cloud.png";
+    weather_img.src="./assets/cloud.png";
     break;
     case'Clear':
-    weather_img.src="./assests/clear.png";
+    weather_img.src="./assets/clear.png";
     break;
     case'Rain':
-    weather_img.src="./assests/rain.png";
+    weather_img.src="./assets/rain.png";
     break;
     case'Mist':
-    weather_img.src="./assests/mist.png";
+    weather_img.src="./assets/mist.png";
     break;
     case'Snow':
-    weather_img.src="./assests/snow.png";
+    weather_img.src="./assets/snow.png";
     break;
   }
 }
